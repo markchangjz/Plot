cfd662bae9d5d27110049cdc08bfb878569b0c50
MarkChang <markchang-17955@email.esunbank.com.tw>
2023-03-09 15:11:55 +0800

    Plotting the Pie Chart

--- Plot/ViewController.swift
+++ Plot/ViewController.swift
@@ -6,14 +6,163 @@
 //
 
 import UIKit
+import CorePlot
 
 class ViewController: UIViewController {
-
+    
+    private lazy var hostView: CPTGraphHostingView = {
+        let view = CPTGraphHostingView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let rates: [(name: String, value: Float)] = [
+        (name: "USD", value: 1.00),
+        (name: "GBP", value: 0.71),
+        (name: "EUR", value: 0.92),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(hostView)
+        NSLayoutConstraint.activate([
+            hostView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            hostView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            hostView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            hostView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        initPlot()
+    }
+    
+    func initPlot() {
+        configureHostView()
+        configureGraph()
+        configureChart()
+        configureLegend()
+    }
+    
+    func configureHostView() {
+        hostView.allowPinchScaling = false
+    }
+    
+    func configureGraph() {
+        // 1 - Create and configure the graph
+        let graph = CPTXYGraph(frame: hostView.bounds)
+        hostView.hostedGraph = graph
+        graph.paddingLeft = 0.0
+        graph.paddingTop = 0.0
+        graph.paddingRight = 0.0
+        graph.paddingBottom = 0.0
+        graph.axisSet = nil
+        
+        // 2 - Create text style
+        let textStyle: CPTMutableTextStyle = CPTMutableTextStyle()
+        textStyle.color = CPTColor.black()
+        textStyle.fontName = "HelveticaNeue-Bold"
+        textStyle.fontSize = 16.0
+        textStyle.textAlignment = .center
+        
+        // 3 - Set graph title and text style
+        graph.title = "USD exchange rates\n2023/03/09"
+        graph.titleTextStyle = textStyle
+        graph.titlePlotAreaFrameAnchor = CPTRectAnchor.top
+    }
+    
+    func configureChart() {
+        // 1 - Get a reference to the graph
+        let graph = hostView.hostedGraph!
+        
+        // 2 - Create the chart
+        let pieChart = CPTPieChart()
+        pieChart.delegate = self
+        pieChart.dataSource = self
+        pieChart.pieRadius = (min(hostView.bounds.size.width, hostView.bounds.size.height) * 0.7) / 2
+        pieChart.identifier = NSString(string: graph.title!)
+        //        pieChart.startAngle = CGFloat(M_PI_4)
+        pieChart.startAngle = CGFloat(Double.pi / 4)
+        pieChart.sliceDirection = .clockwise
+        pieChart.labelOffset = -0.6 * pieChart.pieRadius
+        
+        // 3 - Configure border style
+        let borderStyle = CPTMutableLineStyle()
+        borderStyle.lineColor = CPTColor.white()
+        borderStyle.lineWidth = 2.0
+        pieChart.borderLineStyle = borderStyle
+        
+        // 4 - Configure text style
+        let textStyle = CPTMutableTextStyle()
+        textStyle.color = CPTColor.white()
+        textStyle.textAlignment = .center
+        pieChart.labelTextStyle = textStyle
+        
+        // 5 - Add chart to graph
+        graph.add(pieChart)
+    }
+    
+    func configureLegend() {
+        // 1 - Get graph instance
+        guard let graph = hostView.hostedGraph else { return }
+        
+        // 2 - Create legend
+        let theLegend = CPTLegend(graph: graph)
+        
+        // 3 - Configure legend
+        theLegend.numberOfColumns = 1
+        theLegend.fill = CPTFill(color: CPTColor.white())
+        let textStyle = CPTMutableTextStyle()
+        textStyle.fontSize = 18
+        theLegend.textStyle = textStyle
+        
+        // 4 - Add legend to graph
+        graph.legend = theLegend
+        if view.bounds.width > view.bounds.height {
+            graph.legendAnchor = .right
+            graph.legendDisplacement = CGPoint(x: -20, y: 0.0)
+            
+        } else {
+            graph.legendAnchor = .bottomRight
+            graph.legendDisplacement = CGPoint(x: -8.0, y: 8.0)
+        }
+    }
+}
 
-
+extension ViewController: CPTPieChartDataSource, CPTPieChartDelegate {
+    
+    func numberOfRecords(for plot: CPTPlot) -> UInt {
+        return UInt(rates.count)
+    }
+    
+    func number(for plot: CPTPlot, field fieldEnum: UInt, record idx: UInt) -> Any? {
+        return rates[Int(idx)].value
+    }
+    
+    func dataLabel(for plot: CPTPlot, record idx: UInt) -> CPTLayer? {
+        let value = rates[Int(idx)].value
+        let layer = CPTTextLayer(text: String(format: "\(rates[Int(idx)].name)\n%.2f", value))
+        layer.textStyle = plot.labelTextStyle
+        return layer
+    }
+    
+    func sliceFill(for pieChart: CPTPieChart, record idx: UInt) -> CPTFill? {
+        switch idx {
+        case 0:
+            return CPTFill(color: CPTColor(componentRed:0.92, green:0.28, blue:0.25, alpha:1.00))
+        case 1:
+            return CPTFill(color: CPTColor(componentRed:0.06, green:0.80, blue:0.48, alpha:1.00))
+        case 2:
+            return CPTFill(color: CPTColor(componentRed:0.22, green:0.33, blue:0.49, alpha:1.00))
+        default:
+            return nil
+        }
+    }
+    
+    func legendTitle(for pieChart: CPTPieChart, record idx: UInt) -> String? {
+        return rates[Int(idx)].name
+    }
 }
 
